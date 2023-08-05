58ed04d6be3d4fc6dc7b15e4d36a76be8e9dfafa
tinkerbug <114777377+tinkerbug@users.noreply.github.com>
2023-08-05 11:16:19 -0400

    Removed unnecessary inputs

--- ESP32-Rover-LoRa/inputs.h
+++ ESP32-Rover-LoRa/inputs.h
@@ -2,7 +2,3 @@
 const char* ssid = "XXXXXX";
 const char* password = "XXXXXX";
 
-// IP address of the base station ESP32
-// This is printed to USB when the base station runs
-// attached to a serial port
-IPAddress serverAddress(192, 168, 86, 50);
