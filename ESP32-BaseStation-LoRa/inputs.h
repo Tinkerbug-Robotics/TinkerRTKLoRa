19ba8967f52843f5f7563f9400d9514c9b22d589
tinkerbug <114777377+tinkerbug@users.noreply.github.com>
2024-10-28 14:21:00 -0400

    Delete ESP32-BaseStation-LoRa directory

--- ESP32-BaseStation-LoRa/inputs.h
+++
@@ -1,8 +0,0 @@
-// Replace with your network credentials
-const char* ssid = "XXXXXXX";
-const char* password = "XXXXXXX";
-
-// IP address of the base station ESP32
-// This is printed to USB when the base station runs
-// attached to a serial port
-IPAddress serverAddress(192, 168, 86, 50);
